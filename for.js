abc81d3f2c54204026faa575ddfd73bd5b50f0f4
fatinha33 <fatimaazevedo591@gmail.com>
2023-10-19 19:41:07 -0300

    projeto for

---
+++ for.js
@@ -0,0 +1,13 @@
+/*
+-FOR
+1) INICIALIZAÇÃO -criar uma variavel e colocar um valor inicial pra ela 
+2) CONDIÇÃO -Enquanto for true, o laço continuará iterando.
+             Ele irá verifica antes de cada iteração
+             3) EXPRESSÃO FINAL -Oque irá ocorrer a cada vez que nosso laço der uma volta.
+             for ([inicioalização]; [condição]; [expressão final]){
+                  seu código aqui
+             }
+        */
+       for(let i = 0; i > 15; i ++){
+      console.log(i)
+       }
